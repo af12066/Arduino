c5bfa2d5ed66072ca978302f0f4ff0faf9a31a7d
bced17 <af12066@shibaura-it.ac.jp>
2015-06-08 15:52:34 +0900

    xbeeLedBlink_coordinatorをデータレコーダー用に改造

--- xbeeLedBlink_coordinator/xbeeLedBlink_coordinator.ino
+++ xbeeLedBlink_coordinator/xbeeLedBlink_coordinator.ino
@@ -1,12 +1,38 @@
+const int trig01 = 3;
+const int trig02 = 4;
+const int button = 2;
+
+int i, j;
+
+boolean trigSwitch01 = true;
+boolean trigSwitch02 = false;
+
 void setup() {
+  pinMode(trig01, OUTPUT);
+  pinMode(trig02, OUTPUT);
+  pinMode(button, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print('H');
-  delay(10000);
-  Serial.print('L');
-  delay(10000);
-  // Serial.print('K');
-  // delay(2000);
+  while ( digitalRead(button) == LOW ){}
+  delay(5000);
+  
+  digitalWrite(trig01, trigSwitch01);
+  
+  for (i=0; i<2; i++) {
+    for (j=1; j<6; j++) {
+      Serial.print(j);
+      trigSwitch02 = !trigSwitch02;
+      digitalWrite(trig02, trigSwitch02);
+    
+      delay(5000);
+    }
+  }
+  
+  trigSwitch01 = !trigSwitch01;
+  digitalWrite(trig01, trigSwitch01);
+  
+  for(;;);
+
 }
